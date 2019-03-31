f01f26d7c17248aab57be5527ccd8ca3c51e3b8d
Milen John Thomas <Milen@localhost.localdomain>
2019-03-31 13:46:45 -0400

    chaged

--- security-group-check/main.py
+++ security-group-check/main.py
@@ -4,11 +4,11 @@ from config import conf
 class Authentication:
 
     def __init__(self,profile):
-        self.session = boto3.Session(profile_name=profile)
+        self.session = boto3.Session(profile_name=profile,region_name='us-west-1')
     def initializeService(self,service):
         self.client = self.session.client(service)
         return self.client
 
 account_one = Authentication('default')
-result = account_one.initializeService(conf['service']).SecurityGroup('sg-928dd2e8')
+result = account_one.initializeService(conf['service']).describe_security_groups(GroupIds=['sg-928dd2e8'])
 print(result)
